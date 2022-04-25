8765c558f5092fd726cd3ae2144eb86a0b67b970
Wes Burningham <wesb@qualtrics.com>
2022-04-25 08:26:59 -0600

    Adding Rebecca Handwriting Cask.

---
+++ Casks/font-rebecca-handwriting-font.rb
@@ -0,0 +1,9 @@
+cask "font-rebecca-handwriting-font" do
+  version "0.0.2"
+  sha256 "abd7fae712d616e3be7dcb3602158804b24d3b43a3870ad472d6aeea8b24a562"
+
+  url "https://github.com/wburningham/fonts/releases/download/v#{version}/RebeccaHandwriting.zip"
+  name "Rebecca Handwriting"
+
+  font "Rebecca Handwriting.ttf"
+end
